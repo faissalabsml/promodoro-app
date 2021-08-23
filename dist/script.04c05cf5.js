7ff9a0c5433d15b361bd4529971224ede9f607b4
faissalabsml <faissalabs@gmail.com>
2021-08-23 19:48:28 +0100

    Make the timer element bigger

--- dist/script.04c05cf5.js
+++ dist/script.04c05cf5.js
@@ -616,7 +616,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45979" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43337" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
